88230ccc291e71bc2fc32c6d9e8d5d3f4c385b3f
[RyosukeHiguchi] <[ryosukhiguchi@gmail.com]>
2019-09-14 13:55:37 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
+Rails.application.routes.draw do 
+   devise_for :users
+   root 'pages#home'
+ 
+   devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
