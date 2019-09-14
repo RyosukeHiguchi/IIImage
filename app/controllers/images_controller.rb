0fdcbd86690b67ff2310ec52a1589f17a5d3afdc
[RyosukeHiguchi] <[ryosukhiguchi@gmail.com]>
2019-09-14 22:22:34 +0900

    add images show page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,15 @@
 class ImagesController < ApplicationController
+    def show
+        @image = Image.find_by(params[:id])
+    end
+
     def new
         @image = Image.new
     end
 
     def create
         @image = current_user.images.build(image_params)
-        if @images.save
+        if @image.save
             redirect_to image_path(@image.id), notice: "Tシャツを追加しました"
         else
             render :new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
    root 'pages#home'
    get '/upload', to:'images#new'
    resources :images, only: [:index, :show, :create]
+
+   get '/@:username', to: 'users#show', as: :profile
  
    devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
